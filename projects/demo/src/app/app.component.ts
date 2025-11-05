1b1aabe857e467caa8d01f7058035a48b91a3e66
Ryan Culp <rculp@modusoperandi.com>
2025-11-05 15:28:24 +0000

    add demo project

---
+++ projects/demo/src/app/app.component.ts
@@ -0,0 +1,16 @@
+/**
+ * @license MIT
+ * @copyright Copyright 2025 Modus Operandi Inc. All Rights Reserved.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'demo';
+}
